141481a730d79e6a8f2fc341ba422651b026382d
fsd <webtila.web@gmail.com>
2021-08-18 17:08:04 +0545

    Counter Added

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -12,22 +12,24 @@
         </div> -->
 
         <h1 class=" text-primary bg-white"> Nepal Police Hospital</h1>
-        <h2 class="pt-2 text-white bg-dark"> Sick Book Record</h2>      
+        <h2 class="pt-2 text-dark"> Sick Book Record</h2>      
         <hr>  
- <a href="/inservice" 
-    class="btn btn-primary btn-block btn-vlg "data-toggle="tooltip" 
-    data-placement="top" title="बहालवाला प्रहरी कर्मचारी">Inservice <span class="ml-5 btn text-primary bg-white">{{$ins_count}}</span>  </a>
- 
-<a href="/infamily" 
-    class="btn btn-secondary btn-block btn-vlg"data-toggle="tooltip"
-    data-placement="top"title="बहालवाला प्ररी कर्मचारी परिवार">Infamily <span class="ml-5 btn text-secondary bg-white">{{$inf_count}}</span></a>
-<br><hr>
-<a href="/exservice" 
-    class="btn btn-danger btn-block btn-vlg"data-toggle="tooltip" 
-    data-placement="top" title="भुतपर्व प्रहरी कर्मचारी">ExService <span class="ml-5 btn text-danger bg-white"> {{$exs_count}}</span></a>
-<a href="/exfamily"
-    class="btn btn-warning btn-block btn-vlg"data-toggle="tooltip"
-     data-placement="top"title="भुतपुर्व प्रहरी कर्मचारी परिवार">Exfamily <span class="ml-5 btn text-warning bg-dark"> {{$exf_count}}</span></a>
+        <div>
+            <a href="/inservice" 
+                class="btn btn-primary  btn-vlg "data-toggle="tooltip" 
+                data-placement="top" title="बहालवाला प्रहरी कर्मचारी">Inservice <span class="ml-5 btn text-primary bg-white">{{$ins_count}}</span>  </a>
+            
+            <a href="/infamily" 
+                class="btn btn-secondary  btn-vlg"data-toggle="tooltip"
+                data-placement="top"title="बहालवाला प्ररी कर्मचारी परिवार">Infamily <span class="ml-5 btn text-secondary bg-white">{{$inf_count}}</span></a>
+            <br><hr>
+            <a href="/exservice" 
+                class="btn btn-danger  btn-vlg"data-toggle="tooltip" 
+                data-placement="top" title="भुतपर्व प्रहरी कर्मचारी">ExService <span class="ml-5 btn text-danger bg-white"> {{$exs_count}}</span></a>
+            <a href="/exfamily"
+                class="btn btn-warning  btn-vlg"data-toggle="tooltip"
+                data-placement="top"title="भुतपुर्व प्रहरी कर्मचारी परिवार">Exfamily <span class="ml-5 btn text-warning bg-dark"> {{$exf_count}}</span></a>
+        </div>
 
  </div>
 @endsection
